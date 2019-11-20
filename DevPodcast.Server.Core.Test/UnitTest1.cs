31b2fea5909cd015ee7a82c60c8db773f037f66e
Matthieu Leonard <matthieu.leonard@cdon.com>
2019-11-20 07:53:31 +0100

    Clean up unused projects. Add test  project.  Add appsettings.json

---
+++ DevPodcast.Server.Core.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DevPodcast.Server.Core.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
