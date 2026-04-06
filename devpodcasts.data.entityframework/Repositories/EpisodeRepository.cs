c98b246503d8a7796557c8c47e04021622049f62
mattleon09 <mattleondev@gmail.com>
2026-04-06 17:07:31 +0200

    Add files to git ignore and update episode repo

--- devpodcasts.data.entityframework/Repositories/EpisodeRepository.cs
+++ devpodcasts.data.entityframework/Repositories/EpisodeRepository.cs
@@ -65,24 +65,6 @@ namespace devpodcasts.Data.EntityFramework.Repositories
                 .ToListAsync();
         }
 
-        public override Task<List<Episode>> GetAllAsync()
-        {
-            return _context.Set<Episode>()
-                .Include(e => e.Podcast)
-                .Include(e => e.Tags)
-                .Include(e => e.Categories)
-                .ToListAsync();
-        }
-
-        public override Task<List<Episode>> GetAllAsync(Expression<Func<Episode, bool>> predicate)
-        {
-            return _context.Set<Episode>().Where(predicate)
-                .Include(e => e.Podcast)
-                .Include(e => e.Tags)
-                .Include(e => e.Categories)
-                .ToListAsync();
-        }
-
         public Task<List<Episode>> GetByShowIdAsync(Guid ShowId)
         {
             return _context.Set<Episode>().Where(x => x.PodcastId == ShowId)
